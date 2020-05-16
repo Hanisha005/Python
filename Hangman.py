d5f53c10ad8d89247c025497e6c4789dddeaa83c
Hanisha005 <65460738+Hanisha005@users.noreply.github.com>
2020-05-16 22:51:43 +0530

    Hangman Python
    Hangman, a game where the player has to guess a word, letter by letter, in a limited number of attempts. Make a program that plays Hangman with you – and good luck with the guessing!

---
+++ Hangman.py
@@ -0,0 +1,61 @@
+# Python
+# Hangman game python code with User Menu
+import random
+
+word_list = ['python', 'java', 'kotlin', 'javascript']
+x = random.choice(word_list)
+y = ""
+z = 0
+b = ""
+for i in range(0, len(x)):
+    y = y + "-"
+print("H A N G M A N")
+option = input('Type "play" to play the game, "exit" to quit:')
+while True:
+    if option == "play":
+        print("")
+        print(y)
+        while z < 8 and "-" in y:
+            word = input("Input a letter: ")
+            if len(word) != 1:
+                print("You should print a single letter")
+                if z != 8:
+                    print("")
+                    print(y)
+                continue
+            elif not word.islower():
+                print("It is not an ASCII lowercase letter")
+                if z != 8:
+                    print("")
+                    print(y)
+                continue
+            elif word in y or word in b:
+                print("You already typed this letter")
+                if z != 8:
+                    print("")
+                    print(y)
+            elif word.islower() and len(word) == 1:
+                if word in x and word not in y:
+                    for i in range(0, len(x)):
+                        if x[i] == word:
+                            y = y[:i] + word + y[i + 1:]
+                    if z != 8:
+                        print("")
+                        print(y)
+                    continue
+                else:
+                    print("No such letter in the word")
+                    z = z + 1
+                    b = b + word
+                    if z != 8:
+                        print("")
+                        print(y)
+                    continue
+        if x == y:
+            print("You survived!")
+            break
+        elif z == 8:
+            print("You are hanged!")
+            break
+    elif option == "exit":
+        break
